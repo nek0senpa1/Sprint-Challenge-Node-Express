830a7a2c2e649948a1098edef7731da93e01b9d9
amberm <nekosenpai@gmail.com>
2019-02-22 10:47:56 -0700

    have CRUDed the project DB

--- softserver.js
+++ softserver.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const actionsDB = require('./data/helpers/actionModel');
+const actionDB = require('./data/helpers/actionModel');
 const projectDB = require('./data/helpers/projectModel');
 
-const db1 = require ('./data/dbConfig');
+//const db1 = require ('./data/dbConfig');
 
 const softserver = express();
 
@@ -19,6 +19,66 @@ softserver.get('/', (req,res) => {
         <h3>They drink a ExpressJS drink</h3>
         `);
 });
+
+
+softserver.get('/project', async (reck, rez) => {
+    try{
+        const thing = await projectDB.get(reck.id);
+        // you guys are horrible for this one... how were we supposed to
+        // know what . note to use here... this was a guess...
+        rez.status(200).json(thing);
+    }
+    catch (errerz) {
+        console.log(errerz);
+        rez.status(500).json({
+            message: "You ain\n't got nothin... ",
+        })
+    }
+});
+
+softserver.post('/project', async (rec, rez) => {
+    try{
+        const widget = await projectDB.insert(rec.body);
+        rez.status(201).json(widget)
+    }
+    catch{
+        rez.status(500).json({ message:'Nope! Shatever you sent in body is wrong!'})
+    }
+});
+
+softserver.delete('/project/:id', async (rec, rez) => {
+    try {
+        var widget = await projectDB.remove(rec.params.id);
+        if (widget) {
+            console.log(widget);
+            rez.status(200).json({ message: 'Yup! You Killed It'});
+        } 
+        else {
+            console.log(widget);
+            rez.status(404).json({ message: 'Nothing here to kill...'});
+        }
+        }
+        catch{
+            rez.status(500).json({ message:'You have goofed really hard if you see this. Try a real id number...'})
+        } 
+})
+
+softserver.put('/project/:id', async (rec, rez) => {
+    try {
+        let putPutter = await projectDB.update(rec.params.id, rec.body);
+        if (putPutter) {
+            rez.status(200).json({message: "Good Job, You updated a Thingy"});
+        }
+        else {
+            rez.status(404).json({message: "NOPE! Not a real ID to update"});
+        }
+    }
+    catch{
+        rez.status(500).json({ message:'You have goofed really hard if you see this. Try a real id number...'})
+    }
+})
+
+
 
 
 module.exports = softserver;
